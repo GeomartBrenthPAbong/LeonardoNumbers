e780c6f51e78b0e08a8107a3107a2d1bfd8c44cb
Geomart Brenth Abong <n2txheen@gmail.com>
2016-02-09 20:10:52 +0800

    Fixed the annoying way to create array and tree.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var exports = module.exports = {}
 exports.SequenceGenerators = require('./lib/leonardo_numbers');
 exports.LeonardoIterators = require('./lib/leonardo_iterators');
 
-exports.ds = {}
-exports.ds.LeonardoTree = require('./lib/leonardo_tree');
-exports.ds.LeonardoArray = require('./lib/leonardo_array');
+exports.ds = {
+    LeonardoTree: require('./lib/leonardo_tree'),
+    LeonardoArray: require('./lib/leonardo_array')
+};
